1a8bc96d3b07aced2352fb0e1ecf3af39f0b42a1
Jani Nevalainen <jani.nevalainen@live.fi>
2015-12-16 15:23:38 +0200

    IOT Hub stuff added, not yet wired to work

--- CanMonitor/MainPage.xaml.cs
+++ CanMonitor/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices.Client;
 using GIS = GHIElectronics.UWP.Shields;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,6 +26,9 @@ namespace CanMonitor
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private static int MESSAGE_COUNT = 5;
+        private const string DeviceConnectionString = "<replace>";
+
         private GIS.FEZHAT hat;
         private DispatcherTimer timer;
         private bool next;
@@ -31,16 +38,20 @@ namespace CanMonitor
         {
             this.InitializeComponent();
             Setup();
+
         }
 
         private async void Setup()
         {
+
             this.hat = await GIS.FEZHAT.CreateAsync();
 
             this.timer = new DispatcherTimer();
             this.timer.Interval = TimeSpan.FromMilliseconds(100);
             this.timer.Tick += this.OnTick;
             this.timer.Start();
+
+            //var deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Http1);
         }
 
         private void OnTick(object sender, object e)
@@ -63,6 +74,61 @@ namespace CanMonitor
                 this.next = !this.next;
             }
 
+        }
+
+        public async static Task Start()
+        {
+            try
+            {
+                DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Http1);
+
+                await SendEvent(deviceClient);
+                await ReceiveCommands(deviceClient);
+
+                Debug.WriteLine("Exited!\n");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error in sample: {0}", ex.Message);
+            }
+        }
+
+        static async Task SendEvent(DeviceClient deviceClient)
+        {
+            string dataBuffer;
+
+            Debug.WriteLine("Device sending {0} messages to IoTHub...\n", MESSAGE_COUNT);
+
+            for (int count = 0; count < MESSAGE_COUNT; count++)
+            {
+                dataBuffer = string.Format("Msg from UWP: {0}_{1}", count, Guid.NewGuid().ToString());
+                Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+                Debug.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), count, dataBuffer);
+
+                await deviceClient.SendEventAsync(eventMessage);
+            }
+        }
+
+        static async Task ReceiveCommands(DeviceClient deviceClient)
+        {
+            Debug.WriteLine("\nDevice waiting for commands from IoTHub...\n");
+            Message receivedMessage;
+            string messageData;
+
+            while (true)
+            {
+                receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    Debug.WriteLine("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData);
+
+                    await deviceClient.CompleteAsync(receivedMessage);
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
         }
     }
 }
